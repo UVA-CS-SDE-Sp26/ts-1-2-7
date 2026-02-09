62a562ec06679fc2dc9fc6a86dfd51e7fe65f858
gmenz <cmr8du@virginia.edu>
2026-02-08 21:26:12 -0500

    Team Member A

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,11 @@
+public class UserInterface {
+    public void displayInterface(){
+        //prints out file list
+    }
+    public void displayInterface(int fileNumber){
+        //displays corresponding file and runs default cipher
+    }
+    public void displayInterface(int fileNumber, int cipher){
+        //displays corresponding file and runs corresponding cipher
+    }
+}
