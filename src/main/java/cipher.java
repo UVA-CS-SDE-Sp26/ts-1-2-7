e9456dd9d501f81bf7d63afc24791bb5db574094
katewalldorf <jtu2jm@virginia.edu>
2026-02-12 13:00:22 -0500

    KW 2/12

--- src/main/java/cipher.java
+++ src/main/java/cipher.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class cipher {
     private String decipheredText = "";
     private String cipheredText;
-    private String currentLetter;
     private String keyInput;
     private String keyOutput;
 
@@ -13,23 +12,34 @@ public class cipher {
 
     public cipher(File cipheredMessage, File key) {
         try (Scanner reader = new Scanner (cipheredMessage)) {
-            try(Scanner keyReader = new Scanner(key)) {
-                cipheredText = reader.next();
-                keyInput = keyReader.next();
-                keyOutput = keyReader.next();
-                for (int i = 0; i < cipheredText.length(); i++) {
-                    currentLetter += cipheredText.charAt(i);
-                    inputIndex = keyInput.indexOf(currentLetter);
-                    decipheredText += keyOutput.charAt(inputIndex);
-                    currentLetter = "";
-                }
-            }
-            catch (FileNotFoundException e) {
-                System.out.println("Key not found");
-            }
+            cipheredText = reader.nextLine();
+            System.out.println(cipheredText);
         }
-        catch(FileNotFoundException e) {
+        catch (FileNotFoundException e) {
             System.out.println("Ciphered text file not found");
+        }
+        try(Scanner keyReader = new Scanner(key)) {
+            keyInput = keyReader.next();
+            keyOutput = keyReader.next();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Key not found");
+        }
+        for (int i = 0; i < cipheredText.length(); i++) {
+           /* currentLetter += cipheredText.charAt(i);
+            inputIndex = keyInput.indexOf(currentLetter);
+            System.out.println(currentLetter);
+            decipheredText += keyOutput.charAt(inputIndex);
+            currentLetter = "";
+            */
+            char letter = cipheredText.charAt(i);
+            int inputIndex = keyInput.indexOf(letter);
+            if(inputIndex != -1) {
+                decipheredText += keyOutput.charAt(inputIndex);
+            }
+            else {
+                decipheredText += letter;
+            }
         }
     }
 
