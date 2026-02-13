97384316abc9ea0ff183c859740d17be761b5011
katewalldorf <katewalldorf@gmail.com>
2026-02-13 13:57:44 -0800

    KW 2/13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         userInterface.runProgram(args);
         // ProgramControl program = new ProgramControl(args);
         // program.connector(args);
-
+        File key = new File("./ciphers/key.txt");
+        File test = new File("./ciphers/Tester.txt");
+        cipher Cipher = new cipher(test, key);
+        String decipheredText = Cipher.toString();
     }
 }
