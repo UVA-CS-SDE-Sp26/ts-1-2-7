1948e175d7795d65b5c5fafa25d7ba13ded4f531
katewalldorf <katewalldorf@gmail.com>
2026-02-13 10:07:04 -0800

    KW 2/13

--- src/test/java/cipherTest.java
+++ src/test/java/cipherTest.java
@@ -9,7 +9,7 @@ class cipherTest {
     @Test
     void testToString() {
         File key = new File("./ciphers/key.txt/");
-        File test = new File("./ciphers/KWtester.txt/");
+        File test = new File("./ciphers/Tester.txt/");
         cipher Cipher = new cipher(test, key);
         String actualdecipheredText = Cipher.toString();
         assertEquals("Kate Walldorf", actualdecipheredText);
